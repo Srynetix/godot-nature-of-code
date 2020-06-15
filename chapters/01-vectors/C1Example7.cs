a20caa53ba92484a98e7ef42e335efedbbd78910
Denis BOURGE <bourge.denis@gmail.com>
2020-06-15 18:49:50 +0200

    feat: better C1Example7

--- chapters/01-vectors/C1Example7.cs
+++ chapters/01-vectors/C1Example7.cs
@@ -6,37 +6,39 @@ Motion 101 (velocity)
 */
 
 public class C1Example7 : Node2D {
-    public class Mover {
-        private Vector2 position;
+    public class Mover : Node2D {
         private Vector2 velocity;
 
-        public Mover(Vector2 size) {
-            position = new Vector2((float)GD.RandRange(0, size.x), (float)GD.RandRange(0, size.y));
+        public override void _Ready() {
+            var size = GetViewport().Size;
+            Position = new Vector2((float)GD.RandRange(0, size.x), (float)GD.RandRange(0, size.y));
             velocity = new Vector2((float)GD.RandRange(-2.0f, 2.0f), (float)GD.RandRange(-2.0f, 2.0f));
         }
 
-        public void Update(Vector2 size) {
-            position += velocity;
-            WrapEdges(size);
+        public override void _Process(float delta) {
+            Position += velocity;
+            WrapEdges();
         }
 
-        private void WrapEdges(Vector2 size) {
-            if (position.x > size.x) {
-                position.x = 0;
-            } else if (position.x < 0) {
-                position.x = size.x;
+        private void WrapEdges() {
+            var size = GetViewport().Size;
+
+            if (Position.x > size.x) {
+                Position = new Vector2(0, Position.y);
+            } else if (Position.x < 0) {
+                Position = new Vector2(size.x, Position.y);
             }
 
-            if (position.y > size.y) {
-                position.y = 0;
-            } else if (position.y < 0) {
-                position.y = size.y;
+            if (Position.y > size.y) {
+                Position = new Vector2(Position.x, 0);
+            } else if (Position.y < 0) {
+                Position = new Vector2(Position.x, size.y);
             }
         }
 
-        public void Draw(Node2D node) {
-            node.DrawCircle(position, 20, Colors.Black);
-            node.DrawCircle(position, 18, Colors.LightGray);
+        public override void _Draw() {
+            DrawCircle(Vector2.Zero, 20, Colors.Black);
+            DrawCircle(Vector2.Zero, 18, Colors.LightGray);
         }
     }
 
@@ -47,15 +49,7 @@ public class C1Example7 : Node2D {
         GD.Randomize();
         VisualServer.SetDefaultClearColor(Colors.White);
         
-        mover = new Mover(GetViewport().Size);
-    }
-
-    public override void _Process(float delta) {
-        mover.Update(GetViewport().Size);
-        Update();
-    }
-
-    public override void _Draw() {
-        mover.Draw(this);
+        mover = new Mover();
+        AddChild(mover);
     }
 }
