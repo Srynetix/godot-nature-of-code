5bda09250dc16c1a7d067222775f80a073ce3408
Denis BOURGE <bourge.denis@gmail.com>
2020-07-19 18:09:23 +0200

    c3e1

---
+++ chapters/03-oscillation/C3Example1.cs
@@ -0,0 +1,33 @@
+using Godot;
+
+public class C3Example1 : Node2D, IExample {
+  float angle = 0;
+  float angularVelocity = 0;
+  float angularAcceleration = 0.01f;
+  float lineSize = 50;
+  float ballRadius = 10;
+
+  public string _Summary() {
+    return "Example 3.1:\n"
+      + "Angular motion using rotate()";
+  }
+
+  public override void _Draw() {
+    DrawLine(Vector2.Left * lineSize, Vector2.Right * lineSize, Colors.White, 1, true);
+    DrawCircle(Vector2.Left * lineSize, ballRadius, Colors.LightBlue);
+    DrawCircle(Vector2.Right * lineSize, ballRadius, Colors.LightBlue);
+  }
+
+  public override void _Ready() {
+    Position = GetViewport().Size / 2;
+  }
+
+  public override void _Process(float delta) {
+    angularVelocity += angularAcceleration * delta;
+    angle += angularVelocity;
+
+    Rotation = angle;
+
+    Update();
+  }
+}
