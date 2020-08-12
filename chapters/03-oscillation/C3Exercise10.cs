e99d4faca7ea8b98d83671a2efd314fa64b19c05
Denis BOURGE <denis.bourge@sharingcloud.com>
2020-08-12 19:07:14 +0200

    feat: C3Exercise10

---
+++ chapters/03-oscillation/C3Exercise10.cs
@@ -0,0 +1,47 @@
+using Godot;
+
+public class C3Exercise10 : Node2D
+{
+  public class NoiseWave : SimpleWave
+  {
+    private OpenSimplexNoise noise;
+
+    public NoiseWave()
+    {
+      noise = new OpenSimplexNoise();
+    }
+
+    public override float ComputeY(float angle)
+    {
+      return Utils.Map(noise.GetNoise1d(angle * 10f), -1, 1, -Amplitude, Amplitude);
+    }
+  }
+
+  public override void _Ready()
+  {
+    var size = GetViewportRect().Size;
+
+    var wave1 = new SimpleWave();
+    wave1.Separation = 8;
+    wave1.Length = 150;
+    wave1.Position = new Vector2(size.x / 8, size.y / 4);
+    wave1.Amplitude = 75;
+    AddChild(wave1);
+
+    var wave2 = new NoiseWave();
+    wave2.Separation = 4;
+    wave2.Length = 300;
+    wave2.Position = new Vector2(size.x / 2, size.y / 2);
+    wave2.Amplitude = 100;
+    wave2.AngularVelocity = 0.25f;
+    AddChild(wave2);
+
+    var wave3 = new SimpleWave();
+    wave3.Separation = 8;
+    wave3.StartAngleFactor = 10;
+    wave3.Length = 150;
+    wave3.Position = new Vector2(size.x - size.x / 8, size.y - size.y / 4);
+    wave3.Amplitude = 50;
+    AddChild(wave3);
+  }
+}
---
+++ scripts/common/SimpleWave.cs
@@ -0,0 +1,37 @@
+using Godot;
+
+public class SimpleWave : Node2D
+{
+  public float Radius = 30;
+  public int Separation = 24;
+  public float AngularVelocity = 0.1f;
+  public float StartAngle = 0;
+  public float StartAngleFactor = 1;
+  public float Length = 300;
+  public float Amplitude = 100;
+
+  public virtual float ComputeY(float angle)
+  {
+    return Utils.Map(Mathf.Sin(angle), -1, 1, -Amplitude, Amplitude);
+  }
+
+  public override void _Draw()
+  {
+    float angle = StartAngle;
+
+    for (float x = -Length / 2; x <= Length / 2; x += Separation)
+    {
+      var target = new Vector2(x, ComputeY(angle));
+
+      DrawCircle(target, Radius, Colors.LightBlue.WithAlpha(100));
+      DrawCircle(target, Radius - 2, Colors.White.WithAlpha(100));
+      angle += AngularVelocity;
+    }
+  }
+
+  public override void _Process(float delta)
+  {
+    StartAngle += delta * StartAngleFactor;
+    Update();
+  }
+}
