b6fe648fc92065ab1e1ff81f65b5af155eb49c75
Denis BOURGE <denis.bourge@sharingcloud.com>
2020-08-13 17:00:32 +0200

    feat: C3Example10

---
+++ chapters/03-oscillation/C3Example10.cs
@@ -0,0 +1,21 @@
+using Godot;
+
+public class C3Example10 : Node2D, IExample
+{
+  public string _Summary()
+  {
+    return "Example 3.10:\n"
+      + "Swinging Pendulum\n\n"
+      + "You can move the pendulum by touching it.";
+  }
+
+  public override void _Ready()
+  {
+    var size = GetViewportRect().Size;
+    var pendulum = new SimplePendulum();
+    pendulum.Angle = Mathf.Pi / 4;
+    pendulum.RopeLength = size.y / 2;
+    pendulum.Position = new Vector2(size.x / 2, 0);
+    AddChild(pendulum);
+  }
+}
---
+++ scripts/common/SimplePendulum.cs
@@ -0,0 +1,76 @@
+using Godot;
+
+public class SimplePendulum : Node2D
+{
+  public float AngularVelocity;
+  public float AngularAcceleration;
+  public float Gravity = 0.4f;
+  public float Damping = 0.995f;
+  public float RopeLength = 200;
+  public float Radius = 30;
+  public float Angle;
+  public bool UserControllable = true;
+
+  private bool touched = false;
+  private int touchIndex = -1;
+
+  private Vector2 GetBallPosition()
+  {
+    return new Vector2(RopeLength * Mathf.Sin(Angle), RopeLength * Mathf.Cos(Angle));
+  }
+
+  public override void _Input(InputEvent @event)
+  {
+    if (@event is InputEventScreenTouch eventScreenTouch)
+    {
+      if (eventScreenTouch.Pressed && touchIndex == -1)
+      {
+        if (eventScreenTouch.Position.DistanceTo(GlobalPosition + GetBallPosition()) < Radius * 2)
+        {
+          touchIndex = eventScreenTouch.Index;
+          touched = true;
+        }
+      }
+
+      if (!eventScreenTouch.Pressed && touchIndex == eventScreenTouch.Index)
+      {
+        touched = false;
+        touchIndex = -1;
+      }
+    }
+
+    else if (@event is InputEventScreenDrag eventScreenDrag)
+    {
+      if (touched)
+      {
+        // Compute angle from touch position
+        var touchAngle = (eventScreenDrag.Position - Position).Normalized().Angle();
+        Angle = -(touchAngle - Mathf.Pi / 2);
+      }
+    }
+  }
+
+  public override void _Process(float delta)
+  {
+    if (!touched)
+    {
+      AngularAcceleration = -(Gravity / RopeLength) * Mathf.Sin(Angle);
+      AngularVelocity += AngularAcceleration;
+      Angle += AngularVelocity;
+
+      AngularVelocity *= Damping;
+    }
+
+    Update();
+  }
+
+  public override void _Draw()
+  {
+    var target = GetBallPosition();
+    DrawLine(Vector2.Zero, target, Colors.LightGray);
+    DrawCircle(target, Radius, Colors.LightBlue);
+
+    var ballColor = touched ? Colors.LightGoldenrod : Colors.White;
+    DrawCircle(target, Radius - 2, ballColor);
+  }
+}
