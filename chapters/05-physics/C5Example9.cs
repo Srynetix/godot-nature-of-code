8da23ae2732122f1e09eeb4a25d86096cd6d621e
Denis BOURGE <bourge.denis@gmail.com>
2020-08-24 18:20:42 +0200

    feat: example 5.9

---
+++ chapters/05-physics/C5Example9.cs
@@ -0,0 +1,71 @@
+using Godot;
+
+public class C5Example9 : Node2D, IExample
+{
+  public string _Summary()
+  {
+    return "Example 5.9:\n"
+      + "Collision Listening";
+  }
+
+  public class CollisionBall : Physics.SimpleBall
+  {
+    public bool Colliding = false;
+
+    public CollisionBall()
+    {
+      ContactMonitor = true;
+      ContactsReported = 1;
+    }
+
+    public override void _Ready()
+    {
+      base._Ready();
+
+      Connect("body_entered", this, nameof(OnBodyEntered));
+      Connect("body_exited", this, nameof(OnBodyExited));
+    }
+
+    public void OnBodyEntered(PhysicsBody2D body)
+    {
+      Colliding = true;
+    }
+
+    public void OnBodyExited(PhysicsBody2D body)
+    {
+      Colliding = false;
+    }
+
+    public override void _Draw()
+    {
+      // Detect if colliding
+      var color = BaseColor;
+      if (Colliding)
+      {
+        color = Colors.Red;
+      }
+
+      var outlineVec = new Vector2(OutlineWidth, OutlineWidth);
+      DrawCircle(Vector2.Zero, Radius, OutlineColor);
+      DrawCircle(Vector2.Zero, Radius - OutlineWidth, color);
+    }
+  }
+
+  public override void _Ready()
+  {
+    var size = GetViewportRect().Size;
+    var floor = new Physics.SimpleWall();
+    floor.BodySize = new Vector2(size.x, 100);
+    floor.Position = new Vector2(size.x / 2, size.y);
+    AddChild(floor);
+
+    var spawner = new Physics.SimpleTouchSpawner();
+    spawner.Spawner = (position) =>
+    {
+      var ball = new CollisionBall();
+      ball.GlobalPosition = position;
+      return ball;
+    };
+    AddChild(spawner);
+  }
+}
