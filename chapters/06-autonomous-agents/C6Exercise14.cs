1bbdc60fea2cf2f04a264e61a0d5d90892e3ac02
Denis BOURGE <bourge.denis@gmail.com>
2020-09-04 12:35:15 +0200

    feat: exercise 6.14

---
+++ chapters/06-autonomous-agents/C6Exercise14.cs
@@ -0,0 +1,73 @@
+using Godot;
+using Agents;
+using Physics;
+using System.Collections.Generic;
+
+namespace Examples.Chapter6
+{
+  /// <summary>
+  /// Exercise 6.14: Vehicle Force Factors.
+  /// </summary>
+  public class C6Exercise14 : Node2D, IExample
+  {
+    public string _Summary()
+    {
+      return "Exercise 6.14:\nVehicle Force Factors";
+    }
+
+    private int vehicleCount = 50;
+    private List<SimpleVehicle> vehicles = new List<SimpleVehicle>();
+
+    private class RandomVehicle : RoundVehicle
+    {
+      private float t = 0;
+
+      public override void _Process(float delta)
+      {
+        t += delta * 4;
+
+        // Force factors depends on time
+        SeparationForceFactor = MathUtils.Map(Mathf.Sin(t), -1, 1, 0.5f, 1.5f);
+        SeekForceFactor = 2 - SeparationForceFactor;
+
+        while (t > Mathf.Pi * 2)
+        {
+          t -= Mathf.Pi * 2;
+        }
+
+        base._Process(delta);
+      }
+
+    }
+
+    public override void _Ready()
+    {
+      var size = GetViewportRect().Size;
+      var path = new SimplePath();
+      path.Points.Add(new Vector2(size.x * 1 / 4, size.y * 1 / 4));
+      path.Points.Add(new Vector2(size.x * 3 / 4, size.y * 1 / 4));
+      path.Points.Add(new Vector2(size.x * 3 / 4, size.y * 3 / 4));
+      path.Points.Add(new Vector2(size.x * 1 / 4 - 20, size.y * 3 / 4));
+      path.Looping = true;
+      AddChild(path);
+
+      var spawner = new SimpleTouchSpawner();
+      spawner.SpawnFunction = (pos) =>
+      {
+        var vehicle = new RandomVehicle();
+        vehicle.VehicleGroupList = vehicles;
+        vehicle.Position = pos;
+        vehicle.TargetPath = path;
+        vehicle.SeparationEnabled = true;
+        vehicles.Add(vehicle);
+        return vehicle;
+      };
+      AddChild(spawner);
+
+      for (int i = 0; i < vehicleCount; ++i)
+      {
+        spawner.SpawnBody(MathUtils.RandVector2(0, size.x, 0, size.y));
+      }
+    }
+  }
+}
