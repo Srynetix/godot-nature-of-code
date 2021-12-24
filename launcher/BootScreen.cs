8ba48cc77014d5bf597316fbce7df573b6739a0e
Denis BOURGE <bourge.denis@gmail.com>
2021-12-24 10:04:10 +0100

    wip

--- launcher/BootScreen.cs
+++ launcher/BootScreen.cs
@@ -5,16 +5,16 @@ using Godot;
 /// </summary>
 public class BootScreen : Control
 {
-    private AnimationPlayer animationPlayer;
+	private AnimationPlayer animationPlayer;
 
-    public override void _Ready()
-    {
-        animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-        animationPlayer.Connect("animation_finished", this, nameof(LoadLauncher));
-    }
+	public override void _Ready()
+	{
+		animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		animationPlayer.Connect("animation_finished", this, nameof(LoadLauncher));
+	}
 
-    private void LoadLauncher(string animationName)
-    {
-        GetTree().ChangeScene("res://launcher/Launcher.tscn");
-    }
+	private void LoadLauncher(string _animationName)
+	{
+		GetTree().ChangeScene("res://launcher/Launcher.tscn");
+	}
 }
